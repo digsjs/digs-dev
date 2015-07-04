af8f795f246923e6fd891d93346b832c6130ebed
Christopher Hiller <boneskull@boneskull.com>
2015-07-03 21:34:04 -0700

    stub specification for lib/symlink module

---
+++ test/unit/lib/symlink.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('commands', function () {
+  describe.skip('symlink', function () {
+
+    it('should symlink all files specified in package.json', function () {
+
+    });
+
+    it('should symlink all modules', function () {
+
+    });
+  });
+});
