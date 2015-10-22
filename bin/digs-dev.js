89bb08fe31fde13ff4c674a5f897046ce99e8a67
Christopher Hiller <boneskull@boneskull.com>
2015-10-22 13:22:20 -0700

    general cleanup

--- bin/digs-dev.js
+++ bin/digs-dev.js
@@ -2,11 +2,14 @@
 
 'use strict';
 
-let yargs = require('yargs');
-let path = require('path');
-let execute = require('../lib');
+const yargs = require('yargs');
+const execute = require('../lib');
+const _ = require('lodash');
+const pkg = require('../package.json');
+const emoji = require('node-emoji');
+const chalk = require('chalk');
 
-// note to self: "var" is intended here
+// not a mistake
 /* eslint no-var:0 */
 var argv;
 
@@ -19,48 +22,44 @@ function commandYargs(y, name, options) {
 }
 
 argv = yargs
-  .usage('Usage: $0 <command>')
+  .usage(`Usage: $0 <command>`)
   .command('symlink',
-  `Symlink digs-dev's dotfiles into current or target directory`,
-  function symlink(y) {
-    argv = commandYargs(y, 'symlink');
-  })
+    `Symlink digs-dev's dotfiles into current or target directory`,
+    _.partialRight(commandYargs, 'symlink'))
   .command('upgrade',
-  `Install/upgrades digs-dev's dependencies into current or target directory.` +
-  `(will modify your "package.json" if the directory is under Git VC)`,
-  function upgrade(y) {
-    argv = commandYargs(y, 'upgrade');
-  })
+    `Install/upgrade digs-dev's devDependencies into current or target ` +
+    `directory (will modify your "package.json" if the directory is under VCS)`,
+    _.partialRight(commandYargs, 'upgrade'))
   .command('install',
-  `Performs an "upgrade", "symlink" & "gitignore"`,
-  function install(y) {
-    argv = commandYargs(y, 'install');
-  })
+    `Performs an "upgrade", "symlink" & "gitignore"`,
+    _.partialRight(commandYargs, 'install'))
   .command('gitignore',
-  `Updates .gitignore to include all symlinked files`,
-  function gitignore(y) {
-    argv = commandYargs(y, 'gitignore');
-  })
+    `Updates .gitignore to include all symlinked files`,
+    _.partialRight(commandYargs, 'gitignore'))
   .option('help', {
     alias: 'h',
     describe: 'Show this help, or specify a command for details',
     example: '$0 --help [command]',
     type: 'string'
   })
-  .version(function getVersion() {
-    return require(path.join(__dirname, '..', 'package.json')).version;
-  })
-  .showHelpOnFail(true)
-  .check(function check(finalArgv) {
-    if (finalArgv.hasOwnProperty('help')) {
-      yargs.showHelp();
+  .version(() => pkg.version)
+  .check((_argv) => {
+    if (_.has(_argv, 'help')) {
+      yargs.showHelp('log');
       return true;
     }
-    if (execute[finalArgv._[0]]) {
-      return true;
+    if (_.isEmpty(_argv._)) {
+      throw new Error(`${emoji.get('exclamation')}  Command is required`);
     }
-    throw new Error('Command is required!');
+    const command = _.first(_argv._);
+    if (!_.has(execute, command)) {
+      throw new Error(`${emoji.get('exclamation')}  Unknown command ` +
+        `"${command}"`);
+    }
+    return true;
   })
+  .epilog(`${emoji.get('bug')}  Problems? ` +
+    chalk.underline(`https://github.com/digsjs/digs-dev/issues/`))
   .argv;
 
 execute(argv._[0], argv);
