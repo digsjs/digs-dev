882e083758a007ee7b26db941ad7b52b69729f14
Christopher Hiller <boneskull@boneskull.com>
2015-11-04 01:16:19 -0800

    update the batch updater script

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -6,19 +6,44 @@
 CWD=`pwd`
 ROOT=${1:-${CWD}}
 cd ${ROOT}
-echo "Looking for Digs-related packages in ${ROOT}..."
+echo "Looking for Digs-related packages in ${ROOT}"
 
-[[ ! *digs* ]]
+[[ ! *digs* ]] && {
+  echo "Can't find anything."
+}
+
+function isValid {
+  local DIR="${1}"
+  [[ -d ${DIR} && \
+    ${DIR} != digs-dev && \
+    -e ${DIR}/package.json && \
+    `grep 'digs-dev install' ${DIR}/package.json` ]] && return 0
+  return 1
+}
+
+function cleanup {
+  npm prune
+  for MODULE in node_modules/*
+  do
+    [[ -h ${MODULE} ]] && npm unlink ${MODULE}
+  done;
+}
+
+function upgrade {
+  npm install --production
+  npm link digs-dev
+  npm install
+  grunt devUpdate:auto
+}
+
 for DIR in *digs*
 do
-  [[ -d ${DIR} && ${DIR} != digs-dev ]] && {
-    cd ${DIR}
-    [[ `grep 'digs-dev install' package.json` ]] && {
-      echo "Upgrading `basename ${DIR}`"
-      npm run prepublish
+   [[ isValid ${DIR} ]] && {
+      cd ${DIR}
+      cleanup
+      upgrade
+      cd -
     }
-    cd ..
-  }
 done
 
 cd ${CWD}
